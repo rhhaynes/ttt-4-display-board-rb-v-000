d143257ed83a9c6ed8cc8d47d844fb90e6324e43
Randall Harris Haynes <harrishaynes@gmail.com>
2018-01-11 16:19:05 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,10 @@
 def display_board(board)
   row = Array.new;
-  row[0] = "#{board[0]}|#{board[1]}|#{board[2]}";
+  row[0] = " #{board[0]} | #{board[1]} | #{board[2]} ";
   row[1] = "-----------";
-  row[2] = "#{board[3]}|#{board[4]}|#{board[5]}";
+  row[2] = " #{board[3]} | #{board[4]} | #{board[5]} ";
   row[3] = "-----------";
-  row[4] = "#{board[6]}|#{board[7]}|#{board[8]}";
+  row[4] = " #{board[6]} | #{board[7]} | #{board[8]} ";
   for i in 0..row.length-1
     puts row[i]
   end
