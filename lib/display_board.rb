413d5ab4f03b17329e1c3acfc364d7689d6e4dc6
Randall Harris Haynes <harrishaynes@gmail.com>
2018-01-11 16:28:04 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,11 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def display_board(board)
+  row = Array.new;
+  row[0] = " #{board[0]} | #{board[1]} | #{board[2]} ";
+  row[1] = "-----------";
+  row[2] = " #{board[3]} | #{board[4]} | #{board[5]} ";
+  row[3] = "-----------";
+  row[4] = " #{board[6]} | #{board[7]} | #{board[8]} ";
+  for i in 0..row.length-1
+    puts row[i];
+  end
+end
